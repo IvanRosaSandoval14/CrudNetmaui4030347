e520615ac3d9bacd32f2f298ab6ba2387ce33cee
Ivan Rosa <Ivan Rosa@DESKTOP-2VJVQ61>
2024-08-13 12:34:41 -0600

    Agregar archivos de proyecto.

---
+++ CrudNetmaui4030347/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudNetmaui4030347
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage(); // Eliminada la segunda instancia
+        }
+    }
+}
---
+++ CrudNetmaui4030347/Clientes.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace CrudNetmaui4030347
+{
+    [Table("cliente")]
+    public class Clientes
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("nombrecliente")]
+        public string? NombreCliente { get; set; }
+
+        [Column("movil")]
+        public string? Movil { get; set; }
+
+        [Column("email")]
+        public string? Email { get; set; }
+    }
+}
